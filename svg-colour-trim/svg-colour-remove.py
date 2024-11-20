dac97d7063c34388aecc73ec0a3633b6dfaf935c
dimitar-seraffimov <dimitar.seraffimov@gmail.com>
2024-11-20 15:32:41 +0000

    Progress with revising. Created a quick svg-colour-remove script, had to quickly clean up some SVGs for a project.

---
+++ svg-colour-trim/svg-colour-remove.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+# load the SVG file
+with open("Group.svg", "r") as file:
+    soup = BeautifulSoup(file, "lxml-xml")  # 'lxml-xml' for parsing XML
+
+# find and remove all elements with a specific white fill
+elements_removed = 0
+for element in soup.find_all(attrs={"fill": "#F5645D"}):
+    element.decompose()
+    elements_removed += 1
+
+# save the "clean" SVG
+with open("output.svg", "w") as file:
+    file.write(str(soup))
+
+print(f"Process completed. Removed {elements_removed} elements.")
